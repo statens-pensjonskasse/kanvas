efd8bc8d279371659dcd67b3f3a52520af924ee3
Jarle Mathiesen <jarle.mathiesen@spk.no>
2020-12-04 15:39:41 +0100

    SPKMASTER-39012: la til logging av requests

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
+const morgan =require('morgan')
+
 const app = express();
+
+app.use(morgan('combined'))
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/admin/ping', function (req, res) {
