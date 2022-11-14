5fd0ef65a3ae7f0da61fff16191bd79b4699dc67
Jarle Mathiesen <jarle.mathiesen@spk.no>
2022-11-14 15:58:37 +0100

    SPKPRODUKT-7211: tilbakestiller default tekst

--- app/hardkodinger/hardkodetCSV.js
+++ app/hardkodinger/hardkodetCSV.js
@@ -1,8 +1,16 @@
 export const hardkodet = `\
-Person;1990;;
+Polise 1;2000;;Aktiv;100%;_200123
+Polise 1;2005;;Aktiv;40%;_200123
+Polise 1;2010;;Konsolidert;0%;_200011
 
-Stilling 1;2010;;
-Stilling 1;2020.04.01;;Stillingsstørrelse: 40%
+Polise 2;2005;;Uførepensjon;60%;_200011
+Polise 2;2010;;Uførepensjon;100%;_200011
 
-Pensjonsvedtak 1;2020.04.01;;Pensjonsgrad: 60%
+HarDelvisPensjon;2000;;Nei
+HarDelvisPensjon;2005;;Ja
+HarDelvisPensjon;2010;;Nei
+HarDelvisPensjon;2010;;Nei
+
+# Farging av tidslinjer
+HarDelvisPensjon;color;grey
 `.split('\n')
